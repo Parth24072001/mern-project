0364f0af7d07cddee0619d049ed57dc770ebce32
parth poshiya <parth.radicalloop@gmail.com>
2024-01-30 10:29:56 +0530

    project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,6 @@
+import "./assets/css/styles.css";
+const App = () => {
+    return <div className=" text-red-500">App</div>;
+};
+
+export default App;
---
+++ frontend/src/modules/admins/components/NotEligibleModal.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NotEligibleModal = () => {
+    return <div>NotEligibleModal</div>;
+};
+
+export default NotEligibleModal;
