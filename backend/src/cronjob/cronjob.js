b80273635a0ca40b575d51fe0b7b3fef755c64ae
parth poshiya <poshiyaparth2022@gmail.com>
2024-03-26 16:00:33 +0530

    add cron job for groop

--- backend/src/cronjob/cronjob.js
+++ backend/src/cronjob/cronjob.js
@@ -1,5 +1,6 @@
 import cron from "node-cron";
 import { Expence } from "../models/expense.model.js";
+import { Group } from "../models/group.model.js";
 
 const task = cron.schedule("59 23 * * *", async () => {
   const thirtyDaysAgo = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000); // 30 days ago
@@ -23,4 +24,27 @@ const task = cron.schedule("59 23 * * *", async () => {
     console.error("Error occurred while deleting expenses:", error);
   }
 });
+const task2 = cron.schedule("59 23 * * *", async () => {
+  const thirtyDaysAgo = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000); // 30 days ago
+
+  try {
+    const groupToDelete = await Group.find({
+      soft_delete: true,
+      updatedAt: { $lte: thirtyDaysAgo },
+    });
+
+    if (groupToDelete.length > 0) {
+      for (const group of groupToDelete) {
+        const updatedAtDate = new Date(group.updatedAt);
+        if (updatedAtDate <= thirtyDaysAgo) {
+          await Group.findByIdAndDelete(group._id);
+        }
+      }
+    } else {
+    }
+  } catch (error) {
+    console.error("Error occurred while deleting expenses:", error);
+  }
+});
 task.start();
+task2.start();
